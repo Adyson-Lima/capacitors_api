5f939fd7eeef5b174e867ed2bf9850f996aac85c
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-11-08 22:31:16 -0300

    19° commit - tdd controler: Consegue excluir um capacitor e retornar status 204? GREEN

--- app/controllers/api/v1/capacitors_controller.rb
+++ app/controllers/api/v1/capacitors_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CapacitorsController < ApplicationController
 
-  before_action :set_capacitor, only: %i[show update] # show update destroy
+  before_action :set_capacitor, only: %i[show update destroy] # show update destroy
 
   def index
     @capacitors = Capacitor.all
@@ -26,6 +26,10 @@ class Api::V1::CapacitorsController < ApplicationController
     else
       render json: @capacitor.errors, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @capacitor.destroy!
   end
 
 private
